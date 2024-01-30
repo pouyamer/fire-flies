fe7ac3eeb78453ac510bb551ef072837cdea5f92
Pouya Moazzezi <pmoazzezi@gmail.com>
2024-01-31 01:30:26 +0330

    add reset rotation when out of bounds

--- src/Types/OutOfBoundsConfigType.ts
+++ src/Types/OutOfBoundsConfigType.ts
@@ -3,6 +3,7 @@ type OutOfBoundsConfigType = {
   // -- if postitioningAfterOutOfBoundsMethod = set
   backFillPosition: CartesianCoordinateSystemType
   resetSpeeds: boolean
+  resetRotation: boolean
 
   // what a firefly do after it gets out of bounds
   postitioningMethod: OutOfBoundsPositioningMethodType
